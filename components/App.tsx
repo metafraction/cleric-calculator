7c59ec7f09c4f29584650dcbe85774784c7d4c53
Scott Herren <me@stwoh.com>
2022-03-11 08:50:45 -0600

    more components, layout

--- components/App.tsx
+++ components/App.tsx
@@ -4,20 +4,41 @@ import { RGThemeProvider } from "@raidguild/design-system";
 import CalculatorForm from "./CalculatorForm";
 import initialState from "../state/initialState";
 import reducer from "../state/reducer";
+import actions from "../state/actions";
 
 const App: React.FC = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const setAmount = (a: string) => {
-    console.log("amount");
+
+  const toggleOption = (group: string, option: string) => {
+    dispatch({
+      type: actions.TOGGLE_OPTION,
+      group,
+      option,
+    });
   };
 
-  const setShares = (s: string) => {
-    console.log("shares");
+  const updateOptionValue = (
+    group: string,
+    option: string,
+    key: string,
+    value: number
+  ) => {
+    dispatch({
+      type: actions.UPDATE_OPTION_VALUE,
+      group,
+      option,
+      updateKey: key,
+      updateValue: value,
+    });
   };
 
   return (
     <RGThemeProvider>
-      <CalculatorForm state={state} dispatch={dispatch} />
+      <CalculatorForm
+        options={state.options}
+        toggleOption={toggleOption}
+        updateOptionValue={updateOptionValue}
+      />
     </RGThemeProvider>
   );
 };
--- components/CalculatorForm.tsx
+++ components/CalculatorForm.tsx
@@ -15,133 +15,32 @@ import {
 } from "@chakra-ui/react";
 import _ from "lodash";
 import { commify } from "ethers/lib/utils";
-import actions from "../state/actions";
-import { specificationOptions } from "../utils/constants";
+import TotalsRow from "./TotalsRow";
+import GroupSubtotals from "./GroupSubTotals";
+import { columns } from "../utils/constants";
 
 interface CalculatorFormProps {
-  state: any;
-  dispatch: any;
+  options: any;
+  toggleOption: (group: string, option: string) => void;
+  updateOptionValue: (
+    group: string,
+    option: string,
+    key: string,
+    value: number
+  ) => void;
 }
 
-const columns: {
-  [key: string]: {
-    title: string;
-    width: string;
-  };
-} = {
-  hours: {
-    title: "Hours",
-    width: "20%",
-  },
-  hourly: {
-    title: "Hourly",
-    width: "20%",
-  },
-  total: {
-    title: "Total",
-    width: "20%",
-  },
-  min_timeline: {
-    title: "Min Weeks",
-    width: "15%",
-  },
-  max_timeline: {
-    title: "Max Weeks",
-    width: "15%",
-  },
-};
-
 const CalculatorForm: React.FC<CalculatorFormProps> = ({
-  state,
-  dispatch,
+  options,
+  toggleOption,
+  updateOptionValue,
   ...props
 }) => {
-  const columnSubTotalForGroup = (group: string, column: string) => {
-    const activeOptions = _.filter(state.options[group], ["active", true]);
-    if (column === "hourly") {
-      return _.size(activeOptions) > 0
-        ? _.meanBy(activeOptions, column).toFixed(2)
-        : 0;
-    }
-    if (column === "total") {
-      return commify(_.sum(_.map(activeOptions, (o) => o.hours * o.hourly)));
-    }
-
-    return _.sumBy(activeOptions, column) || 0;
-  };
-
-  const groupSubtotals = (group: string) => (
-    <Flex w="50%" justify="space-between">
-      {_.map(_.keys(columns), (column) => (
-        <Flex justify="center" align="center" w={columns[column].width}>
-          <Heading size="sm">
-            {(column === "hourly" || column === "total") && "$"}
-            {columnSubTotalForGroup(group, column) || 0}
-          </Heading>
-        </Flex>
-      ))}
-    </Flex>
-  );
-
-  const totalsRow = () => {
-    const flatOptions = {
-      ...state.options.specification,
-      ...state.options.content,
-      ...state.options.design,
-      ...state.options.development,
-    };
-    const allActiveOptions = _.filter(flatOptions, ["active", true]);
-    const totalSum = _.multiply(
-      _.sum(_.map(allActiveOptions, (o: any) => o.hours * o.hourly)),
-      1.3 // TODO this is pretty rudimentary for cleric/guild/pm calc
-    );
-
-    const totalValue = (column: string) => {
-      if (column === "total") return totalSum;
-      if (column === "hourly") return _.meanBy(allActiveOptions, "hourly");
-      return _.sumBy(allActiveOptions, column);
-    };
-
-    // TODO update the maths below here for PM/Cleric/Spoils
-    return (
-      <Flex direction="column" w="50%" h="200px" justify="space-around">
-        <Flex justify="center">
-          <Heading size="sm">
-            Project Manager {"=>"} 10% (${commify(totalValue("total") * 0.1)})
-          </Heading>
-        </Flex>
-        <Flex justify="center">
-          <Heading size="sm">
-            Cleric {"=>"} 10% (${commify(totalValue("total") * 0.1)})
-          </Heading>
-        </Flex>
-        <Flex justify="center">
-          <Heading size="sm">
-            Guild Spoils {"=>"} 10% (${commify(totalValue("total") * 0.1)})
-          </Heading>
-        </Flex>
-
-        <Flex borderTop="1px" borderTopColor="whiteAlpha.400">
-          {_.map(_.keys(columns), (column) => (
-            <Flex justify="center" align="center" w={columns[column].width}>
-              <Heading size="sm">
-                {(column === "hourly" || column === "total") && "$"}
-                {column === "total"
-                  ? commify(totalValue(column))
-                  : totalValue(column) || 0}
-              </Heading>
-            </Flex>
-          ))}
-        </Flex>
-      </Flex>
-    );
-  };
-
   return (
     <Box w="80%" mt="70px" mx="auto">
       <Heading py="20px">Cleric Calculator</Heading>
       <Accordion defaultIndex={[0]} allowMultiple>
-        {_.map(_.keys(state.options), (group) => {
+        {_.map(_.keys(options), (group) => {
           return (
             <AccordionItem key={group}>
               {({ isExpanded }) => (
@@ -153,11 +52,11 @@ const CalculatorForm: React.FC<CalculatorFormProps> = ({
                       align="center"
                       w="100%"
                     >
-                      <Heading size="sm" w="40%">
+                      <Heading size="sm" w="50%">
                         {_.capitalize(group)}
                       </Heading>
                       {isExpanded ? (
-                        <Flex w="50%" justify="space-between">
+                        <Flex w="40%" justify="space-between">
                           {_.map(columns, (c) => (
                             <Flex w={c.width}>
                               <Heading as={Box} size="sm" key={c.title}>
@@ -167,29 +66,26 @@ const CalculatorForm: React.FC<CalculatorFormProps> = ({
                           ))}
                         </Flex>
                       ) : (
-                        groupSubtotals(group)
+                        <GroupSubtotals
+                          group={group}
+                          options={options}
+                          isExpanded
+                        />
                       )}
-
                       <AccordionIcon />
                     </AccordionButton>
                   </Heading>
 
                   <AccordionPanel pb={4}>
-                    {_.map(_.keys(state.options[group]), (option: string) => {
-                      const currentOption = state.options[group][option];
+                    {_.map(_.keys(options[group]), (option: string) => {
+                      const currentOption = options[group][option];
                       return (
                         <Flex align="center" gap={3} my={2} key={option}>
                           <Box w="40%">
                             <Checkbox
                               size="lg"
                               isChecked={currentOption.active}
-                              onChange={() =>
-                                dispatch({
-                                  type: actions.TOGGLE_OPTION,
-                                  group,
-                                  option,
-                                })
-                              }
+                              onChange={() => toggleOption(group, option)}
                             >
                               <Text size="lg">
                                 {_.capitalize(currentOption.title)}
@@ -220,13 +116,12 @@ const CalculatorForm: React.FC<CalculatorFormProps> = ({
                                     value={currentOption[key]}
                                     isDisabled={!currentOption.active}
                                     onChange={(e) =>
-                                      dispatch({
-                                        type: actions.UPDATE_OPTION_VALUE,
+                                      updateOptionValue(
                                         group,
                                         option,
-                                        updateKey: key,
-                                        updateValue: Number(e.target.value),
-                                      })
+                                        key,
+                                        Number(e.target.value)
+                                      )
                                     }
                                   />
                                 )}
@@ -236,14 +131,8 @@ const CalculatorForm: React.FC<CalculatorFormProps> = ({
                         </Flex>
                       );
                     })}
-                    <Flex>
-                      <Flex w="40%" justify="flex-end">
-                        <Heading size="sm" pr={4}>
-                          {`${_.capitalize(group)} Subtotal`}
-                        </Heading>
-                      </Flex>
-                      {groupSubtotals(group)}
-                    </Flex>
+
+                    <GroupSubtotals group={group} options={options} />
                   </AccordionPanel>
                 </>
               )}
@@ -251,13 +140,7 @@ const CalculatorForm: React.FC<CalculatorFormProps> = ({
           );
         })}
       </Accordion>
-
-      <Flex align="flex-end" borderTop="1px" borderTopColor="whiteAlpha.700">
-        <Heading w="40%" textAlign="right" size="md" mb={2} pr={4}>
-          Totals
-        </Heading>
-        {totalsRow()}
-      </Flex>
+      <TotalsRow options={options} />
     </Box>
   );
 };
